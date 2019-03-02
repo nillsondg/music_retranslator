5b540813c5d52be647cac5090155df979ee3756b
Dmitry Gordeev <nillsondg@gmail.com>
2019-03-03 01:45:08 +0300

    rewrote to 1.0 api

--- retranslator_bot.py
+++ retranslator_bot.py
@@ -4,15 +4,17 @@ from config import TOKEN
 
 client = discord.Client()
 
-VOICE_CHANNEL_ID = "342012658687672330"
-ADMIN_BOTS_CHANNEL_ID = "341856656721838081"
-MUSIC_CHANNEL_ID = "550429090776088577"
-VOICE_MUSIC_CHANNEL_ID = "342406273016266752"
-USEBOTS_CHANNEL_ID = "341626998709551104"
+VOICE_CHANNEL_ID = 342012658687672330
+ADMIN_BOTS_CHANNEL_ID = 341856656721838081
+MUSIC_CHANNEL_ID = 550429090776088577
+VOICE_MUSIC_CHANNEL_ID = 342406273016266752
+USEBOTS_CHANNEL_ID = 341626998709551104
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 def youtube_url_validation(url):
     youtube_regex = (
@@ -37,21 +39,30 @@ def soundcloud_url(url):
     return False
 
 
+def check_is_connected(channel):
+    for user in channel.members:
+        if user == client.user:
+            return True
+    return False
+
+
 @client.event
 async def on_message(message):
     print('Message from {0.author}: {0.content}'.format(message))
     if message.author == client.user:
         return
 
-    if message.channel.id != MUSIC_CHANNEL_ID:
+    if message.channel.id != USEBOTS_CHANNEL_ID and message.channel.id != MUSIC_CHANNEL_ID:
         return
+    if not discord.opus.is_loaded():
+        discord.opus.load_opus('/usr/local/Cellar/opus/1.3')
 
     if youtube_url_validation(message.content) or soundcloud_url(message.content):
         print("got music link")
-        if not client.is_voice_connected(message.server):
-            voice = await client.join_voice_channel(client.get_channel(VOICE_MUSIC_CHANNEL_ID))
-        await client.send_message(client.get_channel(USEBOTS_CHANNEL_ID), content="!play " + message.content)
-        await client.add_reaction(message, '\N{grinning face with smiling eyes}')
-
+        if not check_is_connected(client.get_channel(VOICE_CHANNEL_ID)):
+            await client.get_channel(VOICE_CHANNEL_ID).connect()
+        await client.get_channel(USEBOTS_CHANNEL_ID).send(content="!play <" + message.content + ">")
+        # check unicode 6.0 name at https://emojipedia.org
+        await message.add_reaction(u"\N{Thumbs Up Sign}")
 
 client.run(TOKEN)
